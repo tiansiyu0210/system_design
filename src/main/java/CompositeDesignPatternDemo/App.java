11941e5c39d8bf6b8bf02e94e0c721e809d7e41a
tiansiyu0210 <tiansiyu297@gmail.com>
2020-06-30 22:29:22 -0400

    improve Composite design pattern

--- src/main/java/CompositeDesignPatternDemo/App.java
+++ src/main/java/CompositeDesignPatternDemo/App.java
@@ -31,7 +31,7 @@ public class App {
         System.out.println("parent department total salary is: " + parentDept.getSalary());
         System.out.println("child 1 department total salary is: " + childDept1.getSalary());
         System.out.println("child 2 department total salary is: " + childDept2.getSalary());
-
+        System.out.println();
         System.out.println("parent department total employee is: " + parentDept.getCount());
         System.out.println("childDept1 department total employee is: " + childDept1.getCount());
         System.out.println("childDept2 department total employee is: " + childDept2.getCount());
@@ -42,13 +42,23 @@ public class App {
         childDept1.add(e9);
         childDept2.add(e10);
 
+        System.out.println("==========================================");
         System.out.println("===after adding one employee in each child department===");
         System.out.println("parent department total salary is: " + parentDept.getSalary());
         System.out.println("child 1 department total salary is: " + childDept1.getSalary());
         System.out.println("child 2 department total salary is: " + childDept2.getSalary());
-
+        System.out.println();
         System.out.println("parent department total employee is: " + parentDept.getCount());
         System.out.println("childDept1 department total employee is: " + childDept1.getCount());
         System.out.println("childDept2 department total employee is: " + childDept2.getCount());
+
+        System.out.println();
+        System.out.println("==========================================");
+        System.out.println("adda employee into Parent department");
+
+        Employee e11 = new Employee(9001, "e11", 1);
+        parentDept.add(e11);
+        System.out.println("parent department total salary is: " + parentDept.getSalary());
+        System.out.println("parent department total employee is: " + parentDept.getCount());
     }
 }
